9e310907e316ee69d6b0e82e716b0c8a91c9abec
RUDRAPATEL19 <rpatel19@mynbcc.ca>
2025-11-07 13:59:42 -0400

    TestUtil: Enhenced dark and light mode button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
 import Navbar from './components/Navbar';
-
+import TextForm from './components/TextForm';
+ 
 function App() {
+  const [mode, setMode] = useState('light')   // Whether dark mode is enabled or not
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#00183bff'
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtil2" nothing = 'Home' about2 = 'About'/>
+      <Navbar title="TextUtil2" nothing = 'Home' about2 = 'About' mode = {mode} toggleMode={toggleMode}/>
       <div className="container">
-      <About/>
+      <TextForm heading="Enter the text to analyse below" mode={mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,7 +13,6 @@ export default function TextForm(props) {
     }
 
     const handleClearClick = () => {
-        let newText = text.toLowerCase();
         setText('')
     }
     
@@ -24,21 +23,24 @@ export default function TextForm(props) {
     const [text, setText] = useState('');   
     return (
         <>
-        <div className="container">
+        <div className="container" style={{color: props.mode === 'light'?'black' : 'white'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} 
+                style={{backgroundColor: props.mode === 'light'?'white' : 'grey', 
+                        color: props.mode === 'light'?'black' : 'white'}} id="myBox" rows="8">
+            </textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
             <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lowercase</button>
             <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'light'?'black' : 'white'}}>
             <h1>Your text summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter the text to Preview it."}</p>
         </div>
         </>
     )
