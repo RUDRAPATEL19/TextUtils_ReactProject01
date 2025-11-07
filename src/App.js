b8edd1ea5825f5f81572670785e1606656eff931
RUDRAPATEL19 <rpatel19@mynbcc.ca>
2025-11-07 14:51:40 -0400

    TestUtil: props and userState  for alert messages

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
  
 function App() {
-  const [mode, setMode] = useState('light')   // Whether dark mode is enabled or not
+  const [mode, setMode] = useState('light');   // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() =>{
+        setAlert(null);
+    }, 2000)
+  }
 
   const toggleMode = () => {
     if(mode === 'light'){
-      setMode('dark')
-      document.body.style.backgroundColor = '#00183bff'
+      setMode('dark');
+      document.body.style.backgroundColor = '#00183bff';
+      showAlert("Dark mode has been enabled", "success");
     }else{
-      setMode('light')
-      document.body.style.backgroundColor = 'white'
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Dark mode has been enabled", "success");
     }
   }
   return (
     <>
       <Navbar title="TextUtil2" nothing = 'Home' about2 = 'About' mode = {mode} toggleMode={toggleMode}/>
-      <div className="container">
-      <TextForm heading="Enter the text to analyse below" mode={mode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+      <TextForm showAlert={showAlert} heading="Enter the text to analyse below" mode={mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,15 +5,18 @@ export default function TextForm(props) {
     const handleUpClick = () => {
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Converted to Uppercase!", "success");
     }
 
     const handleLoClick = () => {
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Converted to Lowercase!", "success");
     }
 
     const handleClearClick = () => {
         setText('')
+        props.showAlert("Text cleared!", "success");
     }
     
     const handleOnChange = (event) => {
